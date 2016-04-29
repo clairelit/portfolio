77dc4402f47f90d093e6c68405700f2d118d7b62
Claire Murray <clairefromcork@gmail.com>
2016-04-29 16:38:01 +0100

    Added a template
    This time round I decided to start with a template, because I prefer working with something visual.  I might be setting myself up for a fall, but I'm going to try it anyway.  The template is a free one from Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('homepage', { title: 'Express' });
 });
 
+router.get('/welcome', function(req, res, next){
+  res.render('welcome');
+});
+
+
+router.get('/myPortfolio', function(req, res, next){
+  var yourName = req.query.yourName;
+  req.session.userName =yourName;
+  res.render('Hello', {usersName: yourName});
+});
 module.exports = router;

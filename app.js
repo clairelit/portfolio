c246a62c9431ff8d576b6f313bbccd74e5a16cc1
Claire Murray <clairefromcork@gmail.com>
2016-05-18 23:11:57 +0100

    Added node modules for db
    Added mongodb, monk, express-sessions to store session data.  Also included the db from azure to make sure the db works if I'm not running it locally.

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('mongodb://dbuserclaire:litclonmel@ds030719.mlab.com:30719/portfoliodb');
+
+// If I am running locally then use 'mongodb://localhost:27017/test' otherwise
+// look for the environment variable
+var url = process.env.CUSTOMCONNSTR_MongoDB || 'mongodb://dbuserclaire:litclonmel@ds030719.mlab.com:30719/portfoliodb';
+
+/*
+ * Requiring the following package to be able to use sessions.
+ * Need sessions to be able to store user details
+ */
+var session = require('express-session');
+
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +37,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//Including this to get sessions to work
+var expressSessionOptions = {
+  secret:'mySecret',
+  resave: true,
+  saveUninitialized: true
+}
+
+//session middleware - has to be used
+app.use(session(expressSessionOptions));
+
+//This makes the database accessible to the router
+app.use(function(req, res, next){
+  req.db=db;
+  next();
+});
+
+
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var bodyParset = require('body-parser');
+
+var mongoClient = require('mongodb').MongoClient;
+
+
+var url = process.env.CUSTOMCONNSTR_MongoDB || 'mongodb://dbuserclaire:litclonmel@ds030719.mlab.com:30719/portfoliodb';
+
 
 
 /* GET home page. */
